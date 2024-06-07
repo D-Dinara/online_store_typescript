5be0f04e330d26ee84cfbbc16a99b86959a33db5
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2024-06-07 12:11:37 -0400

    Add product Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import Product from "./components/Product";
+import {products} from "./data/products"
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="container mx-auto max-w-2xl pt-5">
+      <Product product={products[0]}/>
     </div>
   );
 }
