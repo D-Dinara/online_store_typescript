c97a7b4616e00079230ef92560fa44267b682f6c
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2024-06-13 09:50:15 -0400

    Implement hide details

--- frontend/src/components/Product.tsx
+++ frontend/src/components/Product.tsx
@@ -6,19 +6,26 @@ interface ProductProps {
 }
 
 const Product = ({ product }: ProductProps) => {
-  const [details, setDetails] = useState(false)
+  const [details, setDetails] = useState(false);
+  const btnBgClassName = details ? 'bg-blue-400' : 'bg-yellow-400';
+  const btnClasses = ['py-2 px-4 border', btnBgClassName]
 
   return (
     <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
       <img src={product.image} className='w-1/6' alt={product.title} />
       <p>{product.title}</p>
       <p className='font-bold'>{product.price}</p>
-      {details && <p>{product.description}</p>}
+      {details && 
+        <div>
+         <p>{product.description}</p>
+         <p>Rate: <span style={{fontWeight: 'bold'}}>{product.rating.rate}</span></p>
+        </div>
+      }
       <button 
-      onClick={() => setDetails(true)}
-      className='py-2 px-4 border bg-yellow-400'
+      onClick={() => setDetails(prev => !prev)}
+      className={btnClasses.join(' ')}
       >
-        Show Details
+       { details ? 'Hide details' : 'Show Details' } 
       </button>
     </div>
   )
