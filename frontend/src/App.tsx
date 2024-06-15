2980238015b6125e3891e19c599fddbd5cb1caba
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2024-06-15 17:34:44 -0400

    Refactor: add useProducts custom hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,13 @@
-import { useEffect, useState } from "react";
 import Product from "./components/Product";
-import axios, { AxiosError } from 'axios';
-import { IProduct } from "./models";
+import { useProducts } from "./hooks/products";
 
 function App() {
-
-  const [products, setProducts] = useState<IProduct[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
-
-  const fetchProducts = async () => {
-    try {
-      setError('');
-      setLoading(true);
-      const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=10')
-      setProducts(response.data);
-      setLoading(false);
-    } catch (e: unknown) {
-      const error = e as AxiosError
-      setLoading(false);
-      setError(error.message)
-    }
-   
-  }
-
-  useEffect(() => {
-    fetchProducts()
-  }, [])
+  const { products, error, loading } = useProducts();
 
   return (
     <div className="grid grid-cols-4 gap-4 mx-auto max-w-6xl pt-5">
-      {loading && <p className="text-center">Loading...</p>}
-      {error && <p className="text-center text-red-600">{ error }</p>}
+      { loading && <p className="text-center">Loading...</p> }
+      { error && <p className="text-center text-red-600">{ error }</p> }
       { products.map(product => <Product product={product} key={product.id}/>) }
     </div>
   );
