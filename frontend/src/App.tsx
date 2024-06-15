3164e27c8bb46a4037b77167304a6feda7c17b41
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2024-06-15 17:52:08 -0400

    Implement Modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import ErrorMessage from "./components/ErrorMessage";
 import Loader from "./components/Loader";
+import Modal from "./components/Modal";
 import Product from "./components/Product";
 import { useProducts } from "./hooks/products";
 
@@ -13,6 +14,7 @@ function App() {
       <div className="grid grid-cols-4 gap-4">
         { products.map(product => <Product product={product} key={product.id}/>) }
       </div>
+      <Modal />
     </div>
     
   );
